0edbae33694f3f8f8b6a0b2036b6f1b840207141
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-09-23 17:16:28 +0300

    Add files via upload

---
+++ src/main/java/ru/job4j/tracker/Battery.java
@@ -0,0 +1,21 @@
+package ru.job4j.tracker;
+
+public class Battery {
+    private int value;
+
+    public Battery(int charge) {
+        this.value = charge;
+    }
+    public void pour(Battery another) {
+        this.value = this.value + another.value;
+        another.value = 0;
+    }
+    public static void main(String[] args) {
+        Battery first = new Battery(2);
+        Battery second = new Battery(3);
+        System.out.println("first : " + first.value + ". second : " + second.value);
+        first.pour(second);
+        System.out.println("first : " + first.value + ". second : " + second.value);
+
+    }
+}
