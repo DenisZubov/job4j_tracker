f33533d80fcd592af065b5d328aeb7a437d698de
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-10-08 18:43:48 +0300

    Add files via upload

---
+++ src/main/java/ru/job4j/tracker/Triangle.java
@@ -0,0 +1,44 @@
+package ru.job4j.tracker;
+
+import static java.lang.Math.sqrt;
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point a, Point b, Point c) {
+        Point first = new Point(0, 0);
+        Point second = new Point(0, 0);
+        Point third = new Point(0, 0);
+    }
+
+        public static double period ( double ab, double ac, double bc){
+            double period = (ab + ac + bc)/2;
+            return period;
+        }
+
+        public boolean exist ( double ab, double ac, double bc) {
+            if (((ab + ac) > bc) && ((ab + bc) > ac) && ((bc + ac) > ab)) {
+                return true;
+            }
+            return false;
+        }
+
+        public static double area(Point first, Point second, Point third){
+            double ab = first.distance(second);
+            double ac = first.distance(third);
+            double bc = second.distance(third);
+            double p = period(ab, ac, bc);
+            double area = sqrt(p * (p - ab) * (p - ac) * (p - bc));
+            return area;
+        }
+    public static void main(String[] args) {
+        Point x = new Point(0, 0);
+        Point y = new Point(4, 0);
+        Point z = new Point(0, 8);
+        double rsl = area(x, y, z);
+        System.out.println(rsl);
+    }
+
+}
